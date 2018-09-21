23f2baad673dfdfa8683b86d153beb2e6497c878
simnh <simon.hilpert@uni-flensburg.de>
2018-09-21 13:30:54 +0200

    Update metadata

--- scripts/build.py
+++ scripts/build.py
@@ -2,7 +2,8 @@ from datapackage_utilities import building
 
 building.infer_metadata(package_name='Bordelum', keep_resources=False,
                         foreign_keys={
-                            'bus': ['dispatchable-generator',
-                                    'volatile-generator', 'demand', 'storage'],
+                            'bus': ['dispatchable-generator', 'shortage',
+                                    'volatile-generator', 'demand', 'excess',
+                                    'battery-storage'],
                             'profile': ['volatile-generator', 'demand'],
-                            'from_to_bus': ['transmission', 'conversion']})
+                            'from_to_bus': ['transshipment']})
