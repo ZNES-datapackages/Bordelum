c70db46e2fc416601ad88f88d2637dbd867dc45f
simnh <simon.hilpert@uni-flensburg.de>
2018-09-21 13:47:25 +0200

    Fix package to work with renpass

--- scripts/build.py
+++ scripts/build.py
@@ -1,9 +1,12 @@
-from datapackage_utilities import building
+try:
+    from datapackage_utilities import building
+except ImportError:
+    raise ImportError("Missing datapackage-utilities package...")
 
 building.infer_metadata(package_name='Bordelum', keep_resources=False,
                         foreign_keys={
-                            'bus': ['dispatchable-generator', 'shortage',
-                                    'volatile-generator', 'demand', 'excess',
+                            'bus': ['dispatchable_generator', 'shortage',
+                                    'volatile_generator', 'demand', 'excess',
                                     'battery-storage'],
-                            'profile': ['volatile-generator', 'demand'],
+                            'profile': ['volatile_generator', 'demand'],
                             'from_to_bus': ['transshipment']})
